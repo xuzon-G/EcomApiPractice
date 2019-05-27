a0234fc9e44f5d1736b78a78aaa6b492d28b287f
xuzon-G <pingpong1435@hotmail.com>
2019-05-27 19:14:51 +0545

    add and update finished

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,14 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Model\Product;
 use Illuminate\Http\Request;
+use App\Http\Requests\ProductRequest;
+
+
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
 
+
 class ProductController extends Controller
 {
+
+  public  function __construct(){
+
+    $this->middleware('auth:api')->except('index','show');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -35,9 +44,22 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+
+        $product= new Product;
+        $product->name=$request->name;
+        $product->detail=$request->description;
+
+        $product->stock=$request->stock;
+        $product->price=$request->price;
+
+        $product->discount=$request->discount;
+        $product->save();
+        return response([
+            'data'=>new ProductResource($product)
+        ],201);
+
     }
 
     /**
@@ -71,7 +93,9 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $request['detail']=$request->description;
+        unset($request['description']);
+        $product->update($request->all());
     }
 
     /**
--- app/Model/Product.php
+++ app/Model/Product.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+    protected $fillable=['name','detail','price','stock','discount'];
     function review()
     {
         return $this->hasMany(Review::class);
